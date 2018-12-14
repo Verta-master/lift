766e7162d0bb794033e4940e2e7b8c16886846d0
Verta-master <vertyagina@gmail.com>
2018-12-14 17:54:10 +0600

    Update 2018-12-14T11:54:00.796Z

--- js/script.js
+++ js/script.js
@@ -5,6 +5,17 @@ jQuery(function($) {
 $('.menu__btn').click(function() {
   $('.menu').toggleClass('menu--opened');
   $('.menu__list').slideToggle();
-})
+});
+  
+$('.sidebar__option').click(function(e) {
+  e.preventDefault();
+  $(this).toggleClass('sidebar__option--up');
+  $(this).next().slideToggle();
+});
+  
+if (window.innerWidth < 766) {
+  $('.sidebar__option').addClass('sidebar__option--up');
+  $('.sidebar__field').slideUp();
+}
 
 });
